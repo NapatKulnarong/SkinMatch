2857ba72410ef72957f2953732e867122dd77494
Nisara Ploysuttipol <nisara.pl@ku.th>
2025-10-07 23:57:32 +0700

    feat: allow Google-linked user profiles

--- backend/core/models.py
+++ backend/core/models.py
@@ -28,6 +28,7 @@ class UserProfile(models.Model):
     date_of_birth = models.DateField(null=True, blank=True) # dd/mm/yyyy
     gender = models.CharField(max_length=20, choices=Gender.choices, null=True, blank=True)
     is_verified = models.BooleanField(default=False)
+    google_sub = models.CharField(max_length=64, null=True, blank=True, unique=True)
 
     #audit
     created_at = models.DateTimeField(auto_now_add=True)
